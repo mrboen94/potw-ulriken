2d56de083a995c6c6c2a457c6bda778d9c77e4a7
Mathias Bøe <mrboen94@gmail.com>
2024-12-10 17:57:54 +0100

    Initial commit

---
+++ src/data/data..ts
@@ -0,0 +1,108 @@
+export type SpotifyData = {
+  submittedBy: string;
+  spotifyLink: string;
+  date: Date;
+}
+
+export const data: SpotifyData[] = [
+  {
+    submittedBy: 'Robert H.',
+    spotifyLink: 'https://open.spotify.com/playlist/6YpTzrhJTmFU6Rod8jF9cN?si=128099240fd74348',
+    date: new Date('2024-04-29'),
+  },
+  {
+    submittedBy: 'Mathias B',
+    spotifyLink: 'https://open.spotify.com/playlist/2TERztrxLwfSBeKtgkFOkt?si=5cM0N68dRGKroifo1l3ZyQ&pi=e-C11SKP2VQEah',
+    date: new Date('2024-05-07'),
+  },
+  {
+    submittedBy: "Ole Martin M.",
+    spotifyLink: 'https://music.youtube.com/playlist?list=PLEsHpXFyhA_QEcisNEG7Znqml_nBJe6FM&si=_kRNbNzuCE2mXnVC',
+    date: new Date('2024-05-13'),
+  },
+  {
+    submittedBy: "Terje E.",
+    spotifyLink: 'https://open.spotify.com/playlist/7oAt9h5bTIQkKwrr4V6awQ?si=f2d335ed831f4199',
+    date: new Date('2024-05-28'),
+  },
+  {
+    submittedBy: "Knut-Erland B.",
+    spotifyLink: 'https://open.spotify.com/playlist/3gAJ25V4WG1TqHHOGF6hUo?si=46b9677c5cd54f54',
+    date: new Date('2024-05-31'),
+  },
+  {
+    submittedBy: "Yvonne H.",
+    spotifyLink: "https://open.spotify.com/playlist/1aZxLxAA2YZmOBLfQNzVeG?si=pg6SfeeUTLCzu2yiypn_cA&pi=e-KBnjjAMvTeqf&nd=1&dlsi=ea70d44ad5e94615",
+    date: new Date('2024-06-17'),
+  },
+  {
+    submittedBy: "Bjørn Erik T.",
+    spotifyLink: "https://open.spotify.com/playlist/1XVtbv2mrnkejwjG08ASmg?si=68d70c967bed4e93",
+    date: new Date('2024-07-02'),
+  },
+  {
+    submittedBy: "Marthe Charlotte C.",
+    spotifyLink: "https://open.spotify.com/playlist/0JZjC1sjyin07bqazX1t07?si=8D5SDvGXSZGH5te8X4A0cA&pi=e-n64wD6RmTKyR",
+    date: new Date('2024-07-02'),
+  },
+  {
+    submittedBy: "Thomas B.",
+    spotifyLink: "https://open.spotify.com/playlist/0JZjC1sjyin07bqazX1t07?si=8D5SDvGXSZGH5te8X4A0cA&pi=e-n64wD6RmTKyR",
+    date: new Date('2024-08-01'),
+  },
+  {
+    submittedBy: "Carl August G.",
+    spotifyLink: "https://open.spotify.com/playlist/2rAYZgtEwkFVSINRpTJJnd?si=6c19fbe443084c98",
+    date: new Date('2024-08-07'),
+  },
+  {
+    submittedBy: "Endre M. L.",
+    spotifyLink: "https://open.spotify.com/playlist/241DQZVO5LF8o6mGsowhDT?si=708cdded1af54887",
+    date: new Date('2024-08-16'),
+  },
+  {
+    submittedBy: "Endre M. L.",
+    spotifyLink: "https://open.spotify.com/playlist/1I4Z4bW1myCS8OrGGYGJ68?si=603dd1968a174430",
+    date: new Date('2024-08-16'),
+  },
+  {
+    submittedBy: "Olav K.",
+    spotifyLink: "https://open.spotify.com/playlist/36qEwNXBW6w94G6BUYsRBa?si=91228c89baf142ad",
+    date: new Date('2024-08-26'),
+  },
+  {
+    submittedBy: "Even Berge S.",
+    spotifyLink: "https://open.spotify.com/playlist/4Ig3Q8HtdqBMRm7hEM60DC?si=9dd69d6e7d1e4f02",
+    date: new Date('2024-09-02'),
+  },
+  {
+    submittedBy: "Jan Peder A.",
+    spotifyLink: "https://open.spotify.com/playlist/2Rye7vvRgmo1pxEkxzJTfv?si=Hjswxc_FRHWqsXNgIfxOjw",
+    date: new Date('2024-09-02'),
+  },
+  {
+    submittedBy: "Petter L.",
+    spotifyLink: "https://open.spotify.com/playlist/1uedl5Zzrr8lo25U14V0Vu?si=deef3252049c4fd0",
+    date: new Date('2024-09-10'),
+  },
+  {
+    submittedBy: "Fredrik Eimhjellen K.",
+    spotifyLink: "https://open.spotify.com/playlist/5nGcbicAy0G8SMxRBeygz3?si=cc9ce08e198d4064",
+    date: new Date('2024-10-15'),
+  },
+  {
+    submittedBy: "Mathias B.",
+    spotifyLink: "https://open.spotify.com/playlist/6UNx2pJzM17hI7iQmKRDv9?si=giRa81EiTvm3oUkE5H30kA&pi=e-8o50WYf7Qdme",
+    date: new Date('2024-11-01'),
+  },
+  {
+    submittedBy: "Hans-Martin Z.",
+    spotifyLink: "https://open.spotify.com/playlist/19jKvTjvFv3KHgFMozE8dC?si=lfCGtwqPQSmwyK5Z67mWDQ&pi=e-2VFNcfd3SUOQ",
+    date: new Date('2024-11-15'),
+  },
+  {
+    submittedBy: "Magne A.",
+    spotifyLink: "https://open.spotify.com/playlist/2yGxj1LwGtk4mor0zOa9lR?si=338d39f7344a4aa5",
+    date: new Date('2024-11-25'),
+  },
+];
