d0cd47ffaff0b1b4d16a6b39d531137bc1d165c5
Mathias Bøe <mrboen94@gmail.com>
2024-12-13 12:59:09 +0100

    removed unused file and fixed date issues

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,13 @@ import PlaylistTable from '@/components/PlaylistTable.tsx';
 import { data, SpotifyData } from '@/data/data.ts';
 import UserChart from '@/components/UserChart.tsx';
 
-const spotifyData: SpotifyData[] = data;
+const rawData: SpotifyData[] = data;
+const spotifyData: SpotifyData[] = [];
 
 // Remove future dates
-spotifyData.forEach((playlist) => {
-  if (new Date(playlist.date) > new Date()) {
-    playlist.date = new Date();
+rawData.forEach((playlist) => {
+  if (new Date(playlist.date) < new Date()) {
+    spotifyData.push(playlist);
   }
 });
 
